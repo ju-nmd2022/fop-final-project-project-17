33af6d419fac5f438452305ce4b396fd652fe344
borcicjovan <bojo22fp@student.ju.se>
2023-05-04 15:35:33 +0200

    Cat movement centered

--- new.js
+++ new.js
@@ -2,9 +2,9 @@
 
 // define the canvas
 function setup() {
-    createCanvas(600, 650);
-    frameRate(35);
-    background(135, 206, 235);
+  createCanvas(600, 650);
+  frameRate(35);
+  background(135, 206, 235);
 }
 
 // variables used
@@ -17,61 +17,63 @@ let flipped = false; // to check the cat image
 
 // images
 function preload() {
-    ground = loadImage('images/ground.png');
-    cat = loadImage('images/cat.png');
-    mountain = loadImage('images/mountain.png');
-    tree = loadImage('images/tree.png');
-    firstCloud = loadImage('images/cloud.png');
-    secondCloud = loadImage('images/cloud2.png');
+  ground = loadImage("images/ground.png");
+  cat = loadImage("images/cat.png");
+  mountain = loadImage("images/mountain.png");
+  tree = loadImage("images/tree.png");
+  firstCloud = loadImage("images/cloud.png");
+  secondCloud = loadImage("images/cloud2.png");
 }
 
 function draw() {
-    // draw background and mountain
-    background(135, 206, 235);
-    tint(100, 128);
-    image(mountain, 240, 200, 580, 450);
+  // draw background and mountain
+  background(135, 206, 235);
+  tint(100, 128);
+  image(mountain, 240, 200, 580, 450);
 
-    // update cloud positions and draw clouds
-    noTint();
-    for (let i = 0; i < cloudX.length; i++) {
-        cloudX[i] -= cloudSpeed; // move the cloud to the left
-        if (cloudX[i] <= -300) { // if cloud is off-screen to the left
-            cloudX[i] = 600; // move cloud to the right side of the canvas
-        }
-        if (i === 0) {
-            image(firstCloud, cloudX[i], 0, 300, 200);
-        } else {
-            image(secondCloud, cloudX[i], 0, 300, 230);
-        }
+  // update cloud positions and draw clouds
+  noTint();
+  for (let i = 0; i < cloudX.length; i++) {
+    cloudX[i] -= cloudSpeed; // move the cloud to the left
+    if (cloudX[i] <= -300) {
+      // if cloud is off-screen to the left
+      cloudX[i] = 600; // move cloud to the right side of the canvas
     }
-
-    // draw ground, tree, cat, and start button
-    noTint();
-    image(ground, -30, 300, 700, 600);
-    tint(200, 255);
-    image(tree, -90, 310, 250, 230);
-    noTint();
-
-    // draw cat and flipped version
-    push();
-    translate(catX + 40, 0);
-    if (flipped) {
-        scale(-1, 1);
+    if (i === 0) {
+      image(firstCloud, cloudX[i], 0, 300, 200);
+    } else {
+      image(secondCloud, cloudX[i], 0, 300, 230);
     }
-    image(cat, 0, 480, 80, 60);
-    pop();
+  }
 
-    // moves and flips the cat depending on key pressed
-    if (keyIsDown(LEFT_ARROW)) {
-        if (catX > 35) { //stops cat from continuing out of canvas
-            catX -= 8;
-            flipped = true;
-        }
-    } else if (keyIsDown(RIGHT_ARROW)){
-        if (catX < width - 115) {
-            catX += 8;
-            flipped = false;
-        }
+  // draw ground, tree, cat, and start button
+  noTint();
+  image(ground, -30, 300, 700, 600);
+  tint(200, 255);
+  image(tree, -90, 310, 250, 230);
+  noTint();
+
+  // draw cat and flipped version
+  push();
+  imageMode(CENTER);
+  translate(catX + 40, 0);
+  if (flipped) {
+    scale(-1, 1);
+  }
+  image(cat, 0, 510, 80, 60);
+  pop();
+
+  // moves and flips the cat depending on key pressed
+  if (keyIsDown(LEFT_ARROW)) {
+    if (catX > 35) {
+      //stops cat from continuing out of canvas
+      catX -= 8;
+      flipped = true;
     }
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    if (catX < width - 115) {
+      catX += 8;
+      flipped = false;
+    }
+  }
 }
-
